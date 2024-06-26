1bd9326a27985797f5bbf399557d700230ade7c1
yarfik <yarfik@gmail.com>
2024-06-26 23:37:06 +0700

    add random quote selector

---
+++ main.py
@@ -0,0 +1,32 @@
+import smtplib
+import datetime as dt
+import random
+
+my_email = "yarfikardiansyah@gmail.com"
+my_password = "eqixlhrbqxdyfgjc"
+to_email = "yarfik97@yahoo.com"
+quotes = []
+
+with open("quotes.txt", "r") as f_quotes:
+    quotes = f_quotes.readlines()
+
+
+def send_email(content):
+    with smtplib.SMTP("smtp.gmail.com", port=587) as conn:
+        conn.starttls()
+        conn.login(user=my_email, password=my_password)
+        conn.sendmail(
+            from_addr=my_email,
+            to_addrs=to_email,
+            msg=f"Subject:Daily Quote\n\n{content}"
+        )
+
+
+current_date = dt.datetime.now()
+day_of_week = current_date.weekday()
+if day_of_week in range(0, 7):
+    random_quote = random.choice(quotes)
+    send_email(random_quote)
+    print(random_quote)
+
+# print(current_date.weekday())
