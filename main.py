d2640931e5c3934d49d98cc2982929dab0082d29
yarfik <yarfik@gmail.com>
2024-06-27 18:55:45 +0700

    finished

--- main.py
+++ main.py
@@ -1,32 +1,39 @@
 import smtplib
 import datetime as dt
 import random
+import pandas as pd
 
 my_email = "yarfikardiansyah@gmail.com"
-my_password = "eqixlhrbqxdyfgjc"
-to_email = "yarfik97@yahoo.com"
-quotes = []
-
-with open("quotes.txt", "r") as f_quotes:
-    quotes = f_quotes.readlines()
+my_password = ""
 
 
-def send_email(content):
+def send_email(destination_name, to_email, content):
     with smtplib.SMTP("smtp.gmail.com", port=587) as conn:
+        msg = f"Subject:Happy Birthday {destination_name}\n\n{content}"
         conn.starttls()
         conn.login(user=my_email, password=my_password)
         conn.sendmail(
             from_addr=my_email,
             to_addrs=to_email,
-            msg=f"Subject:Daily Quote\n\n{content}"
+            msg=msg
         )
 
 
-current_date = dt.datetime.now()
-day_of_week = current_date.weekday()
-if day_of_week in range(0, 7):
-    random_quote = random.choice(quotes)
-    send_email(random_quote)
-    print(random_quote)
+def get_letter(person_name):
+    file_name = f"./letter_templates/letter_{random.randint(1, 3)}.txt"
+    with open(file_name) as letter_file:
+        letter_lines = letter_file.readlines()
+        letter_lines[0] = letter_lines[0].replace("[NAME]", person_name)
+        return "".join(letter_lines)
+    return "- Happy Birthday -"
 
-# print(current_date.weekday())
+
+current_date = dt.datetime.now()
+day_of_week = current_date.day
+month_of_day = current_date.month
+
+df = pd.read_csv("birthdays.csv")
+for person_birth in df.itertuples(index=False, name="Person"):
+    if day_of_week == person_birth.day and month_of_day == person_birth.month:
+        letter = get_letter(person_birth.name)
+        send_email(person_birth.name, person_birth.email, letter)
